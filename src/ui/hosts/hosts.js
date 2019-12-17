bc3e64d38d6f4257d434dd43a52f2d9a452b3bf2
scandinave <romain.lebaro@protonmail.com>
2019-12-17 12:05:35 +0100

    add template for hosts and remote

---
+++ src/ui/hosts/hosts.js
@@ -0,0 +1,31 @@
+#!/usr/bin/gjs
+/* hosts.js
+ *
+ * Copyright 2019 scandinave
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+const { GObject } = imports.gi;
+
+
+var Hosts = GObject.registerClass({
+    GTypeName: 'Hosts',
+    Template: 'resource:///info/scandi/kodimote/ui/hosts/hosts-item.js'
+}, class Hosts extends Gtk.Box {
+
+  _init(params) {
+    super._init(params);
+  }
+});
+
